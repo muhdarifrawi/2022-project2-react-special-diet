f7bd14b4bf36352006b7d176394479ad13beb4a9
arif <muhdarifrawi@gmail.com>
2022-07-25 12:48:46 +0800

    changed stall list format to cards.

--- src/Stalls.js
+++ src/Stalls.js
@@ -1,34 +1,40 @@
 import React from "react";
 import axios from "axios";
 
-export default class Stalls extends React.Component{
+export default class Stalls extends React.Component {
     state = {
         data: []
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
         axios.get("http://localhost:3000/stalls")
-        .then(res =>{
-            console.log(res)
-            let response = res.data
-            this.setState({
-                data:response
+            .then(res => {
+                console.log(res)
+                let response = res.data
+                this.setState({
+                    data: response
+                })
             })
-        })
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <React.Fragment>
                 <div class="container">
                     <h1>Stalls</h1>
-                    <ul>
-                        {this.state.data.map(d =>{
-                            return(
-                                <li>{d.submittedBy}</li>
+                    <div>
+                        {this.state.data.map(d => {
+                            return (
+                                <div class="card m-3" style={{width: "18rem"}}>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{d.stallName}</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">{d.stallType}</h6>
+                                        
+                                    </div>
+                                </div>
                             )
                         })}
-                    </ul>
+                    </div>
                 </div>
             </React.Fragment>
         )
