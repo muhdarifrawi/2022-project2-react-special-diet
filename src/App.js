d75942cd0807936e54def5e39b2108ff16d514e4
arif <muhdarifrawi@gmail.com>
2022-07-25 13:46:41 +0800

    added navigation.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
 import React from "react";
 import './App.css';
-import Stalls from "./Stalls";
+import Stalls from "./components/Stalls";
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>Hello world</h1>
-      <Stalls/>
-    </React.Fragment>
-  );
+
+class App extends React.Component {
+  state = {
+    page: "home"
+  }
+
+
+  changePage =(event)=>{
+    console.log(event.target.innerHTML.toLowerCase())
+    this.setState({
+      page: event.target.innerHTML.toLowerCase()
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <span className="nav-link" value="home" 
+                  data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                  onClick={this.changePage}>Home</span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link" value="stalls" 
+                  data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                  onClick={this.changePage}>Stalls</span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link" 
+                  data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                  onClick={this.changePage}>Contribute</span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link" 
+                  data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show"
+                  onClick={this.changePage}>About Us</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {this.state.page === "home" ? <Home /> : ""}
+        {this.state.page === "stalls" ? <Stalls /> : ""}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
